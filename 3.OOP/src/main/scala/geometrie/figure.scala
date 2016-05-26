d43c1a4e08feaf9fe88acdf03e8e1877fa2c9642
Yves Eugene <yeugene86@yahoo.com>
2016-05-26 11:12:24 -0400

    calculde l'aire

--- 3.OOP/src/main/scala/geometrie/figure.scala
+++ 3.OOP/src/main/scala/geometrie/figure.scala
@@ -5,10 +5,20 @@ package geometrie
   */
 class Point(var x:Int , var y :Int ) {
 
-  def deplacer (dx:Int,dy: Int): Unit = {
+  def translation (dx:Int,dy: Int): Unit = {
     x = x + dx
     y = y + dy
   }
 
  override def toString():String = s"($x,$y)"
+}
+
+ abstract class Figure {
+
+  def aire ( ):Int
+
+ }
+
+class Carre (cote :Int) extends {
+  def aire () :Int = cote*cote
 }
--- 3.OOP/src/main/scala/hw.scala
+++ 3.OOP/src/main/scala/hw.scala
@@ -1,12 +1,25 @@
-import geometrie.Point
+import geometrie.{Carre, Point}
 
 object Hi {
   def main(args: Array[String]) = {
     val a = new Point(0,1)
-    println(a)
+    println(s"a: $a")
 
     val b = new Point (4,1)
-    println (b)
+    println (s"b: $b")
 
+    b.translation(0,2)
+    println(s"b: $b")
+    println(a)
+
+    a.translation(2,1)
+    println(s"a:$a")
+
+    val  carre = new geometrie.Carre (9)
+
+    println(s"carre: ${carre.aire()}")
+
+    val carreBX = new Carre(b.x)
+    println(s"aire bx: ${carreBX.aire()}")
   }
 }
